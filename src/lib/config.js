2decd35ad5a99f20f3f4925ecf4b237ef0e81b6f
Courteous <inumiduncourteous@gmail.com>
2025-11-05 16:28:48 +0100

    production

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
-// const baseURL = 'https://sabino-secondary-schoool-app-backen.vercel.app'; 
-const baseURL = 'http://localhost:8001'; 
+const baseURL = 'https://xo-cleaners.onrender.com'
+// const baseURL = 'http://localhost:8001'; 
 
 export default baseURL;
