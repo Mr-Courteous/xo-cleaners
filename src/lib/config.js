ade61a28b4b1d6973c8d812862294651945dc8e5
Courteous <inumiduncourteous@gmail.com>
2026-02-11 17:40:26 +0100

    editing env eror

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 // const baseURL = 'https://xo-cleaners.onrender.com'
-const baseURL = 'http://localhost:8001';
+// const baseURL = 'http://localhost:8001';
 
 // In your frontend source code
 // const baseURL = "";
